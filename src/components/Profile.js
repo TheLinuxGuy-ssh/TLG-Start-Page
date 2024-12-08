b1087a429f2b59d504fb5dbd512e769224102243
root <root@codeplanet-core.codeplanetstudios.tech>
2024-12-08 08:16:58 +0530

    init

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,10 @@
 import { useSettings } from "@/context/settings"
 
 const Profile = () => {
-    const { settings, setSettings, resetSettings } = useSettings()
+    const { settings } = useSettings()
     return (
     <>
-    <img className={"profile-logo left-1/2 absolute transform -translate-x-1/2 top-6 z-[-1] w-[8rem] shadow shadow-[0_0_60px_-15px_" + settings.profile.glowColor + "]"} style={{boxShadow: "0 0 60px -15px " + settings.profile.glowColor + ";"}} src={settings.profile.dp} />
+    <img className={`profile-logo left-1/2 absolute transform -translate-x-1/2 top-6 z-[-1] w-[8rem] shadow shadow-[0_0_60px_-15px_${settings.profile.glowColor}]`} style={{boxShadow: "0 0 60px -15px " + settings.profile.glowColor + ";"}} src={settings.profile.dp} />
     </>
     );
 };
