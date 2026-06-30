26102b8b0f5ec2d4ffeccd2c54ef2c7b1b2b9427
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-07-01 03:44:52 +0530

    eslint fix

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,10 @@ const Profile = () => {
 	return (
 		<>
 			<img
-				className={(settings.profile.enable ? `` : `hidden`) + ` profile-logo left-1/2 absolute transform -translate-x-1/2 top-6 z-[-1] w-[8rem]`}
+				className={
+					(settings.profile.enable ? `` : `hidden`) +
+					` profile-logo left-1/2 absolute transform -translate-x-1/2 top-6 z-[-1] w-[8rem]`
+				}
 				style={{ boxShadow: "0 0 60px -15px " + settings.profile.glowColor }}
 				src={settings.profile.dp}
 			/>
