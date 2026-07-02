9354ca10c285d6ebf5cf59800c369f28a9413832
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-07-03 00:32:25 +0530

    Profile Image import fix with fetchAsset hook

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,22 @@
 import { useSettings } from "@/context/settings"
+import { fetchAsset } from "@/utils/fetchAsset"
+import { useEffect, useState } from "react"
 
 const Profile = () => {
 	const { settings } = useSettings()
+	const [icon, setIcon] = useState(null)
+	useEffect(() => {
+		// Fetch fetch image
+		fetchAsset(settings.fetch.image)
+			.then((data) => {
+				if (data) {
+					setIcon(data) // Set the image only if there is no warning message
+				}
+			})
+			.catch((error) => {
+				console.error("Failed to fetch image:", error)
+			})
+	}, [settings.fetch.image, setIcon])
 	return (
 		<>
 			<img
@@ -10,7 +25,7 @@ const Profile = () => {
 					` profile-logo left-1/2 absolute transform -translate-x-1/2 top-6 z-[-1] w-[8rem]`
 				}
 				style={{ boxShadow: "0 0 60px -15px " + settings.profile.glowColor }}
-				src={settings.profile.dp}
+				src={icon}
 				alt="profile-img"
 			/>
 		</>
